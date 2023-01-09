2b45bfe84fbb50ffc13c84c138ae5e5c53e1be71
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-01-09 00:45:49 +0000

    italics

--- src/main/java/xyz/amymialee/mialeemisc/util/MialeeText.java
+++ src/main/java/xyz/amymialee/mialeemisc/util/MialeeText.java
@@ -6,6 +6,7 @@ import xyz.amymialee.mialeemisc.MialeeMisc;
 
 import java.util.List;
 
+@SuppressWarnings("unused")
 public class MialeeText {
     public static Text withColor(Text text, int color) {
         Style style = text.getStyle().withColor(color);
@@ -14,6 +15,15 @@ public class MialeeText {
             return styled.get(0);
         }
         MialeeMisc.LOGGER.error("Failed to set color of text: " + text.getString() + " to color: " + color);
+        return text;
+    }
+
+    public static Text withoutItalics(Text text) {
+        List<Text> styled = text.getWithStyle(text.getStyle().withItalic(false));
+        if (styled.size() > 0) {
+            return styled.get(0);
+        }
+        MialeeMisc.LOGGER.error("Failed to remove italics from text: " + text.getString());
         return text;
     }
 }
